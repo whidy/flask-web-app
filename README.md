5615d6c8c6f6afc6c7e4bf8542b8563b0375f5b0
baishun <baishun@wodcloud.com>
2023-06-28 15:48:01 +0800

    my flask web app

---
+++ README.md
@@ -0,0 +1,20 @@
+# Python Web Application
+
+参考：
+
+* https://github.com/xtekky/gpt4free
+* https://flask.palletsprojects.com/en/2.3.x/quickstart/
+* https://liaobots.com/zh
+
+## 运行
+
+```
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+flask --app application run
+```
+
+## 模型
+
+参见：g4f/models.py
---
+++ application.py
@@ -0,0 +1,48 @@
+import g4f
+
+from flask import Flask, request, url_for, render_template, make_response
+from markupsafe import escape
+
+# url_for('static', filename='style.css')
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/hello/<name>")
+def hello_world(name=None):
+    return render_template("hello.html", name=name)
+
+
+@app.route("/gpt", methods=["GET", "POST"])
+def gpt():
+    if request.method == "POST":
+        question = request.form["question"]
+        auth = request.form["auth"] or "4PT9JqBJscqCw"
+        model = request.form["model"] or "gpt-3.5-turbo"
+
+        try:
+            response = g4f.ChatCompletion.create(
+                model=model,
+                provider=g4f.Provider.Liaobots,
+                messages=[{"role": "user", "content": question}],
+                auth=auth,
+            )
+            return {"answer": response}
+
+        except Exception as e:
+            error_msg = str(e)
+            response = make_response({"error": error_msg}, 500)
+            response.headers["Content-Type"] = "application/json"
+            return {"answer": "发生错误，信息：" + response}
+    else:
+        return render_template("gpt.html")
+
+
+@app.route("/about")
+def about():
+    return "The about page, Nothing more but whidy look at you."
